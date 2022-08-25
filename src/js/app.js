8aca20f9c1e1c7e84677bfdb107678015d729617
AdamWojda <wojda1122@gmail.com>
2022-08-25 17:29:09 +0200

    Phase 1: Add mobile nav

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,26 @@ import '../scss/app.scss';
 import './demo.js';
 import './carousel.js';
 import './accordion.js';
+
+
+class App {
+  constructor() {
+    this.mobile_trigger = document.querySelector('.header__mobile__link');
+    this.navigation = document.querySelector('.header__nav');
+    this.init();
+  }
+
+  init() {
+    this.mobile_menu();
+  }
+
+  mobile_menu() {
+    this.mobile_trigger.addEventListener('click', (e) =>{
+      this.navigation.classList.toggle('header__nav--active');
+      this.mobile_trigger.classList.toggle('header__mobile__link--active');
+    })
+  }
+}
+
+new App();
+
